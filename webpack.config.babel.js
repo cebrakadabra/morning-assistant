b9c2fdfe109996f6e30311006dbfa66f4b6b657f
cebrakadabra <christoph.eberl@redbull.com>
2019-10-08 20:59:45 -0700

    update dependencies

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -62,7 +62,7 @@ module.exports = {
         },
     },
     module: {
-        loaders: [
+        rules: [
             {test: /\.js$/, loader: 'babel-loader', exclude: /(node_modules|src\/dist\/vendor\/bitmovin)/},
             {test: /\.css|.less?$/, loader: 'style-loader!css-loader!less-loader', exclude: /node_modules/},
             {test: /\.png|.svg|.jpg|.woff2|.woff|.ttf|.eot?$/, loader: 'file-loader', exclude: /node_modules/},
